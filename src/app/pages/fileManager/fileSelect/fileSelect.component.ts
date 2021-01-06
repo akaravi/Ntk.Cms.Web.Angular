75461af23844479069b5d4d2700cbe6fe30b6589
akaravi <mac.karavi@ntk.ir>
2021-01-06 23:10:45 +0330

    filemanger tested

--- src/app/pages/fileManager/fileSelect/fileSelect.component.ts
+++ src/app/pages/fileManager/fileSelect/fileSelect.component.ts
@@ -10,15 +10,32 @@ import { ComponentOptionFileUploadModel } from 'src/app/core/cmsComponentModels/
 export class FileSelectComponent implements OnInit {
 
   constructor() { }
+;
+  public ajaxSettings: object;
+  public enableRtl: boolean;
+  public hostUrl: string = 'https://ej2-aspcore-service.azurewebsites.net/';
 
-  ngOnInit() {
-    this.optionsFileUpload.onActions = {
-      onActionSelect: (model) => this.onActionSelectFile(model),
-    };
-  }
+
+
+
+
+
+
   uploadedfileName = '';
   uploadedfileKey = '';
   optionsFileUpload=new ComponentOptionFileUploadModel();
+  public ngOnInit(): void {
+    this.optionsFileUpload.onActions = {
+      onActionSelect: (model) => this.onActionSelectFile(model),
+    };
+      this.ajaxSettings = {
+          url: this.hostUrl + 'api/FileManager/FileOperations',
+           getImageUrl: this.hostUrl + 'api/FileManager/GetImage',
+           uploadUrl: this.hostUrl + 'api/FileManager/Upload',
+           downloadUrl: this.hostUrl + 'api/FileManager/Download'
+      };
+      this.enableRtl = true;
+  }
   onActionSelectFile(model: ErrorExceptionResult<FileUploadedModel>): void {
     // console.log('model', model);
     if (model && !model.IsSuccess) {
--- src/app/pages/fileManager/fileSelect/fileSelect.module.ts
+++ src/app/pages/fileManager/fileSelect/fileSelect.module.ts
@@ -2,17 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FileSelectComponent } from './fileSelect.component';
 import { FileUploadModule } from '../fileUpload/fileUpload.module';
+import { FileManagerModule, NavigationPaneService, ToolbarService, DetailsViewService  } from '@syncfusion/ej2-angular-filemanager';
+import 'rxjs/add/operator/map';
+
 
 
 @NgModule({
   imports: [
     CommonModule,
     FileUploadModule,
+    FileManagerModule,
 
   ],
   declarations: [FileSelectComponent],
   exports:[FileSelectComponent],
-  providers: []
+  providers: [NavigationPaneService, ToolbarService, DetailsViewService]
 
 })
 export class FileSelectModule { }
