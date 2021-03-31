bb7c6cf0bb18fe1b5724e543b2d08a745abeacaf
Office <34806399+akaravi@users.noreply.github.com>
2021-03-31 18:01:16 +0430

    update ver

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,13 +1,11 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
-
 import { EnumDeviceType, EnumOperatingSystemType } from 'ntk-cms-api';
-
 export const environment = {
   production: false,
   developing: false,
-  appVersion: '991111',
+  appVersion: '14000111',
   USERDATA_KEY: 'authf649fc9a5f55',
   isMockEnabled: true,
   apiUrl: 'api',
